0366c59a2f3aa79361b08d45dd85a151c86157de
Clive Watts <clive.watts@takealot.com>
2019-02-08 07:59:27 +0200

    Ready for grads

--- app/src/main/java/com/takealot/gradmoviesorter/api/ApiHelper.kt
+++ app/src/main/java/com/takealot/gradmoviesorter/api/ApiHelper.kt
@@ -11,11 +11,14 @@ object ApiHelper {
     open fun doTitleSearch(context: Context, url:String, term:String) {
 
         var useCaseReturn:Search = context as MainActivity
+        //spaces to plus to conform to api
         val spaceSafeTerm = term.replace(" ", "+")
-        val searchUrl = "$url$spaceSafeTerm."
+        val searchUrl = "$url$spaceSafeTerm"
+
         Ion.with(context).load(searchUrl).setLogging("Networking", Log.DEBUG).asJsonObject().withResponse().setCallback { e, result ->
             useCaseReturn.onTitleSearch(result.result)
         }
+
     }
 
 }
